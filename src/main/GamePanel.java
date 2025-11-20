11fd5e4aba9f79f57fd6e5c08568e2cbe6bf9eda
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2025-11-20 15:05:34 -0500

    Create GamePanel.java

---
+++ src/main/GamePanel.java
@@ -0,0 +1,62 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
+    final int FPS = 60;
+    Thread gameThread;
+    PlayManager playManager;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setBackground(Color.black);
+        this.setLayout(null);
+
+        playManager = new PlayManager();
+
+    }
+
+    //We launch the game here, by activating this thread
+    //When a thread starts it automatically calls the run method
+    public void launchGame() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        // Game Loop
+        double drawInterval = 100000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while(gameThread != null) {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime)/drawInterval;
+            lastTime = currentTime;
+            if(delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+        }
+    }
+
+    private void update(){
+        playManager.update();
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+        playManager.draw(g2d);
+    }
+}
