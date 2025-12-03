ca813a05206364c8136741b41a7cb69729a4c346
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2025-12-03 14:44:52 -0500

    added Space Pause Key event Handler

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -6,7 +6,7 @@ import java.awt.event.KeyListener;
 public class KeyHandler implements KeyListener {
 
     // Must be static so all classes can read them
-    public static boolean upPressed, downPressed, leftPressed, rightPressed;
+    public static boolean upPressed, downPressed, leftPressed, rightPressed, pausePressed;
 
     @Override
     public void keyTyped(KeyEvent e) {
@@ -22,8 +22,14 @@ public class KeyHandler implements KeyListener {
         if (code == KeyEvent.VK_A)  leftPressed = true;
         if (code == KeyEvent.VK_S)  downPressed = true;
         if (code == KeyEvent.VK_D)  rightPressed = true;
+        if (code == KeyEvent.VK_SPACE)  {
+            if(pausePressed){
+                pausePressed = false;
+            } else {
+                pausePressed = true;
+            }
+        }
     }
-
     @Override
     public void keyReleased(KeyEvent e) {
 
