c78fc8ac66b538f50b7b19797f6cb84cc4a785bf
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-03-31 17:02:28 -0400

    Update Mino_Bar.java

--- src/mino/Mino_Bar.java
+++ src/mino/Mino_Bar.java
@@ -5,87 +5,47 @@ import java.awt.Color;
 public class Mino_Bar extends Mino {
 
     public Mino_Bar() {
-        create(Color.CYAN); // Bar piece is often cyan
+        create(Color.CYAN);
     }
 
     @Override
     public void setXY(int x, int y) {
-        // DIRECTION 1 (horizontal by default)
-        // 0 1 2 3
-
-        b[0].x = x;
-        b[0].y = y;
-
-        b[1].x = x + Block.SIZE;
-        b[1].y = y;
-
-        b[2].x = x - Block.SIZE;
-        b[2].y = y;
-
-        b[3].x = x - 2 * Block.SIZE;
-        b[3].y = y;
+        // DIRECTION 1 (horizontal): 2 0 1 3  (b[0] is pivot)
+        b[0].x = x;              b[0].y = y;
+        b[1].x = x + Block.SIZE; b[1].y = y;
+        b[2].x = x - Block.SIZE; b[2].y = y;
+        b[3].x = x - 2 * Block.SIZE; b[3].y = y;
     }
 
-    // -------------------------------
-    // DIRECTION 1 (horizontal)
-    // -------------------------------
     @Override
     public void getDirection1() {
-        // 0 1 2 3
-
-        tempB[0].x = b[0].x;
-        tempB[0].y = b[0].y;
-
-        tempB[1].x = b[0].x + Block.SIZE;
-        tempB[1].y = b[0].y;
-
-        tempB[2].x = b[0].x - Block.SIZE;
-        tempB[2].y = b[0].y;
-
-        tempB[3].x = b[0].x - 2 * Block.SIZE;
-        tempB[3].y = b[0].y;
-
-        updateXY(1);
+        // Horizontal: 2 0 1 3
+        tempB[0].x = b[0].x;              tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x + Block.SIZE; tempB[1].y = b[0].y;
+        tempB[2].x = b[0].x - Block.SIZE; tempB[2].y = b[0].y;
+        tempB[3].x = b[0].x - 2 * Block.SIZE; tempB[3].y = b[0].y;
     }
 
-    // -------------------------------
-    // DIRECTION 2 (vertical)
-    // -------------------------------
     @Override
     public void getDirection2() {
+        // Vertical:
+        // 2
         // 0
         // 1
-        // 2
         // 3
-
-        tempB[0].x = b[0].x;
-        tempB[0].y = b[0].y;
-
-        tempB[1].x = b[0].x;
-        tempB[1].y = b[0].y + Block.SIZE;
-
-        tempB[2].x = b[0].x;
-        tempB[2].y = b[0].y - Block.SIZE;
-
-        tempB[3].x = b[0].x;
-        tempB[3].y = b[0].y - 2 * Block.SIZE;
-
-        updateXY(2);
+        tempB[0].x = b[0].x; tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x; tempB[1].y = b[0].y + Block.SIZE;
+        tempB[2].x = b[0].x; tempB[2].y = b[0].y - Block.SIZE;
+        tempB[3].x = b[0].x; tempB[3].y = b[0].y - 2 * Block.SIZE;
     }
 
-    // -------------------------------
-    // DIRECTION 3 (horizontal again)
-    // -------------------------------
     @Override
     public void getDirection3() {
-        getDirection1(); // Bar piece has only 2 unique rotations
+        getDirection1(); // I-piece has 2 unique rotations
     }
 
-    // -------------------------------
-    // DIRECTION 4 (vertical again)
-    // -------------------------------
     @Override
     public void getDirection4() {
-        getDirection2(); // Bar piece has only 2 unique rotations
+        getDirection2();
     }
 }
